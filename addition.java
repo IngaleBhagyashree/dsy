0d07b8ff707d82c499304448d0ebf6e3ae24ef09
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2026-05-24 13:02:27 +0530

    java  add prg

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition{
+    public static void main(String[]args){
+        System.out.println("hello");
+        // int (a,b): (12,3);
+        int a=12,b=12;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
